4b5b3ab61bb01d04778411114493a7ee84a77173
Maheresio <zamalek.545r5@gmail.com>
2025-08-05 15:02:10 +0300

    update cerate setup intent to confirm the payment

--- pages/api/create-setup-intent.ts
+++ pages/api/create-setup-intent.ts
@@ -7,16 +7,25 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Only POST allowed' });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Only POST allowed' });
+  }
 
-  const { customerId } = req.body;
+  const { customerId, cardToken } = req.body;
+
   try {
     const setupIntent = await stripe.setupIntents.create({
       customer: customerId,
+      payment_method: cardToken,
       usage: 'off_session',
+      confirm: true,
     });
-    res.status(200).json({ clientSecret: setupIntent.client_secret });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
+
+    res.status(200).json({
+      clientSecret: setupIntent.client_secret,
+      paymentMethodId: setupIntent.payment_method,
+    });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
   }
 }
